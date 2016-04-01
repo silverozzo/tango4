3baf4df167919b4f12988b3757451efaa84e356a
silverozzo <silverozzo@gmail.com>
2016-04-01 14:38:37 +0600

    added sale offer generating

--- bar/admin.py
+++ bar/admin.py
@@ -2,6 +2,7 @@ from django.contrib             import admin
 from swingtix.bookkeeper.models import BookSet, Account, AccountEntry, Transaction
 
 from bar.models import FoodStuff, FoodProvider, Purchase, CashBox
+from bar.models import Recipe, RecipeIngredient, Product, SaleOffer, Calculation
 
 
 class AccountEntryInline(admin.TabularInline):
@@ -20,6 +21,26 @@ class PurchaseAdmin(admin.ModelAdmin):
 		return ";".join(obj.transactions.values_list('description', flat=True))
 
 
+class RecipeIngredientInline(admin.TabularInline):
+	model = RecipeIngredient
+
+
+class RecipeAdmin(admin.ModelAdmin):
+	model   = Recipe
+	inlines = (RecipeIngredientInline,)
+
+
+class CalculationInline(admin.TabularInline):
+	model = Calculation
+
+
+class SaleOfferAdmin(admin.ModelAdmin):
+	model        = SaleOffer
+	inlines      = (CalculationInline,)
+	list_display = ('product', 'price', 'is_actual')
+	list_filter  = ('is_actual',)
+
+
 admin.site.register(BookSet)
 admin.site.register(Account, AccountAdmin)
 admin.site.register(Transaction)
@@ -28,4 +49,6 @@ admin.site.register(FoodStuff)
 admin.site.register(FoodProvider)
 admin.site.register(Purchase, PurchaseAdmin)
 admin.site.register(CashBox)
-
+admin.site.register(Recipe, RecipeAdmin)
+admin.site.register(Product)
+admin.site.register(SaleOffer, SaleOfferAdmin)
--- bar/models.py
+++ bar/models.py
@@ -1,5 +1,6 @@
 from __future__ import unicode_literals
 
+from decimal                    import Decimal, ROUND_UP
 from django.db                  import models
 from future.utils               import python_2_unicode_compatible
 from swingtix.bookkeeper.models import Account, BookSet, Transaction
@@ -42,20 +43,41 @@ class FoodStuff(models.Model):
 			account.save()
 			self.monetary_account = account
 		return super(FoodStuff, self).save(*args, **kwargs)
+	
+	def get_unit_cost(self, date=None):
+		material_count = self.material_account.balance(date)
+		monetary_count = self.monetary_account.balance(date)
+		if material_count == 0:
+			return 0
+		
+		return monetary_count / material_count
+	
+	def is_feasible(self, date=None):
+		if self.material_account.balance(date) > 0:
+			return True
+		return False
+	
+	def get_material_count(self, date=None):
+		return self.material_account.balance(date)
 
 
-@python_2_unicode_compatible
 class Recipe(models.Model):
 	name = models.CharField(max_length=200)
+	
+	def __unicode__(self):
+		return self.name
 
 
-@python_2_unicode_compatible
 class RecipeIngredient(models.Model):
-	recipe = models.ForeignKey(Recipe)
-	count  = models.DecimalField(max_digits=8, decimal_places=2)
+	recipe  = models.ForeignKey(Recipe, related_name='ingredients')
+	stuff   = models.ForeignKey(FoodStuff)
+	count   = models.DecimalField(max_digits=8, decimal_places=2)
+	comment = models.CharField(max_length=200, blank=True, default='')
+	
+	def __unicode__(self):
+		return self.stuff.name + ' x ' + str(self.count)
 
 
-@python_2_unicode_compatible
 class Product(models.Model):
 	name        = models.CharField(max_length=200)
 	recipe      = models.OneToOneField(Recipe)
@@ -63,22 +85,70 @@ class Product(models.Model):
 	fixed_price = models.DecimalField(max_digits=8, decimal_places=2, default=0)
 	rounding    = models.DecimalField(max_digits=8, decimal_places=2, default=1)
 	markup      = models.DecimalField(max_digits=8, decimal_places=2, default=1)
+	
+	def __unicode__(self):
+		return self.name
+	
+	def prepare_price(self, cost):
+		price = (cost * self.markup) / (Decimal(10) ** self.rounding)
+		price = price.quantize(Decimal(1), rounding=ROUND_UP)
+		price = price * (Decimal(10) ** self.rounding)
+		return price
 
 
-@python_2_unicode_compatible
 class SaleOffer(models.Model):
 	product   = models.ForeignKey(Product)
 	is_actual = models.BooleanField(default=True)
 	price     = models.DecimalField(max_digits=8, decimal_places=2, default=0)
 	when      = models.DateTimeField(auto_now=True)
+	feasible  = models.BooleanField(default=False)
+	
+	def __unicode__(self):
+		return self.product.name
+	
+	def save(self, *args, **kwargs):
+		new_record = False
+		if self.pk is None:
+			new_record = True
+		if self.is_actual:
+			SaleOffer.objects.filter(product=self.product).update(is_actual=False)
+		result = super(SaleOffer, self).save(*args, **kwargs)
+		if not new_record:
+			return result
+		
+		self.feasible = True
+		full_cost     = Decimal(0)
+		for ingr in self.product.recipe.ingredients.all():
+			calc = Calculation.create(self, ingr)
+			if not calc.feasible:
+				self.feasible = False
+			full_cost += calc.monetary_count
+		
+		self.price = self.product.prepare_price(full_cost)
+		
+		result = super(SaleOffer, self).save(*args, **kwargs)
+		
+		return result
 
 
-@python_2_unicode_compatible
 class Calculation(models.Model):
 	sale_offer     = models.ForeignKey(SaleOffer)
 	ingredient     = models.ForeignKey(RecipeIngredient)
 	material_count = models.DecimalField(max_digits=8, decimal_places=2, default=0)
 	monetary_count = models.DecimalField(max_digits=8, decimal_places=2, default=0)
+	feasible       = models.BooleanField(default=False)
+	
+	@staticmethod
+	def create(offer, ingredient):
+		calc = Calculation(
+			sale_offer     = offer,
+			ingredient     = ingredient,
+			material_count = ingredient.count,
+			monetary_count = ingredient.count * ingredient.stuff.get_unit_cost(),
+			feasible       = ingredient.count <= ingredient.stuff.get_material_count()
+		)
+		calc.save()
+		return calc
 
 
 class CashBox(models.Model):
@@ -98,7 +168,6 @@ class CashBox(models.Model):
 		return super(CashBox, self).save(*args, **kwargs)
 
 
-@python_2_unicode_compatible
 class Sale(models.Model):
 	offer        = models.ForeignKey(SaleOffer)
 	cashbox      = models.ForeignKey(CashBox)
@@ -138,14 +207,19 @@ class Purchase(models.Model):
 	unit_size    = models.DecimalField(max_digits=8, decimal_places=2, default=0)
 	
 	def save(self, *args, **kwargs):
-		super(Purchase, self).save(*args, **kwargs)
+		new_record = False
+		if self.id is None:
+			new_record = True
+		
+		result = super(Purchase, self).save(*args, **kwargs)
+		
+		if not new_record:
+			return result
+		
 		full_count = self.unit_count * self.unit_size
 		
 		res = self.stuff.material_account.post(full_count, self.provider.material_account, 
 			'supply from provider to stuff')
-		print(res)
-		print(res[0])
-		print(res[0].transaction)
 		self.transactions.add(res[0].transaction)
 		
 		res = self.stuff.monetary_account.post(self.cost, self.provider.monetary_account, 
@@ -155,3 +229,5 @@ class Purchase(models.Model):
 		res = self.provider.monetary_account.post(self.cost, get_main_cashbox().account,
 			'payment for delivery')
 		self.transactions.add(res[0].transaction)
+		
+		return result
